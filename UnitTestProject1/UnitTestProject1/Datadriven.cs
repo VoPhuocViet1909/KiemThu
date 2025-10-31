182f98b8b2b727e401fb06964f159b0836155289
Nhathoang999 <huynhnhathoang0203@gmail.com>
2025-11-01 03:14:15 +0700

    test

---
+++ UnitTestProject1/UnitTestProject1/Datadriven.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UnitTestProject1
+{
+    [TestClass]
+    public class Datadriven
+    {
+        public TestContext TestContext { get; set; }
+
+        [DataSource(
+            "Microsoft.VisualStudio.TestTools.DataSource.CSV",
+            "|DataDirectory|\\data.csv",
+            "data#csv",
+            DataAccessMethod.Sequential),
+         DeploymentItem("data.csv"),
+         TestMethod]
+
+        public void CheckTriangle_DataDriven_CSV()
+        {
+            
+            int a = Convert.ToInt32(TestContext.DataRow[0]);
+            int b = Convert.ToInt32(TestContext.DataRow[1]);
+            int c = Convert.ToInt32(TestContext.DataRow[2]);
+            string expected = TestContext.DataRow[3].ToString();
+
+            if (expected == "Exception")
+            {
+                Assert.ThrowsException<Exception>(() =>
+                {
+                    CheckTriangle(a, b, c);
+                });
+            }
+            else
+            {
+                bool exp = bool.Parse(expected);
+                bool actual = CheckTriangle(a, b, c);
+                Assert.AreEqual(exp, actual);
+            }
+        }
+
+        // Viết hàm đề cho
+        private bool CheckTriangle(int a, int b, int c)
+        {
+            bool result;
+            if (a <= 0 || b <= 0 || c <= 0)
+                throw new Exception("Cạnh không được nhỏ hơn hoặc bằng 0!");
+            if (a + b > c)
+                if (a + c > b)
+                    if (b + c > a)
+                        result = true;
+                    else
+                        result = false;
+                else
+                    result = false;
+            else
+                result = false;
+
+            return result;
+        }
+    }
+
+}
+
